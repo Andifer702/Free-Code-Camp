edbae06fe427e50eaaeb2c193c34d42ee28a8ed9
Joon Park <apark702@gmail.com>
2016-03-29 16:54:31 -0400

    Create 03. Exact Change.js

---
+++ Advanced-Algorithmic-Scripting/03. Exact Change.js
@@ -0,0 +1,17 @@
+function checkCashRegister(price, cash, cid) {
+  var change;
+  var ans = [];
+  // Here is your change, ma'am.
+  change = cash - price;
+  var p = cid[0];
+  var n = cid[1];
+  var d = cid[2];
+  var q = cid[3];
+  var gw = cid[4];
+  var al = cid[5];
+  var jm = cid[6];
+  var aj = cid[7];
+  var bf = cid[8];
+  
+  return change;
+}
