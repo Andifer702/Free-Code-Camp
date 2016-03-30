f7bdbb0e6ea2f45cca3f26d99e69a5cffd98029d
Joon Park <apark702@gmail.com>
2016-03-30 17:06:36 -0400

    Update 03. Exact Change.js

--- Advanced-Algorithmic-Scripting/03. Exact Change.js
+++ Advanced-Algorithmic-Scripting/03. Exact Change.js
@@ -12,6 +12,19 @@ function checkCashRegister(price, cash, cid) {
   var jm = cid[6];
   var aj = cid[7];
   var bf = cid[8];
-  
+  var denom = [100, 20, 10, 5, 1, 0.25, 0.10, 0.05, 0.01];
   return change;
 }
+
+// Example cash-in-drawer array:
+// [["PENNY", 1.01],
+// ["NICKEL", 2.05],
+// ["DIME", 3.10],
+// ["QUARTER", 4.25],
+// ["ONE", 90.00],
+// ["FIVE", 55.00],
+// ["TEN", 20.00],
+// ["TWENTY", 60.00],
+// ["ONE HUNDRED", 100.00]]
+
+checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
