8f4a04dc3421399801d0a9318fc38f2203ae90da
Joon Park <apark702@gmail.com>
2016-04-01 10:38:04 -0400

    Update 03. Exact Change.js

--- Advanced-Algorithmic-Scripting/03. Exact Change.js
+++ Advanced-Algorithmic-Scripting/03. Exact Change.js
@@ -28,17 +28,21 @@ function checkCashRegister(price, cash, cid) {
   for (var j = 0; j < drawerCash.length; j++){
     if (change/denom[j] >= 1){
       var bill;
-      // if the change is divisible by a denomination keep subtracting the change from cash in drawer until it is no longer divisible or there are no more cash in the drawer.
+      // if the change is divisible by a denomination 
+      //keep subtracting the change from cash in drawer until it is no longer divisible 
+      //Or there are no more cash in the drawer.
       for (var k = 1; change/denom[j] >= 1 && drawerCash[j] > 0; k++){
         drawerCash[j] = drawerCash[j] - denom[j];
         change = (change- denom[j]).toFixed(2);
         bill = k;
       }
-      //
-      if (cid[j][1] > 0){
+      //update the cid value to the amount of change that needs to be given out 
+      //then push the value to the ans along with the currency
+      if (cid[j][1] >= denom[j]*bill){
           cid[j][1] = denom[j]*bill;
           ans.push(cid[j]);
       }
+      //if there is less currency than needed in the drawer, return 'Insufficient Funds'
       else {
         return "Insufficient Funds";
       }
