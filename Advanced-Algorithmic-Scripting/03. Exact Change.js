7fa3bf825a53876ad9120ec85c6154f133502298
Joon Park <apark702@gmail.com>
2016-03-31 21:56:32 -0400

    Update 03. Exact Change.js

--- Advanced-Algorithmic-Scripting/03. Exact Change.js
+++ Advanced-Algorithmic-Scripting/03. Exact Change.js
@@ -3,15 +3,18 @@ function checkCashRegister(price, cash, cid) {
   var drawerCash = [];
   var ans = [];
   // Here is your change, ma'am.
+  //reverse the order of cash in drawer to start giving change from largest denomination
   cid.reverse();
   change = cash - price;
   
+  //set an array for single denominations
   var denom = [100, 20, 10, 5, 1, 0.25, 0.10, 0.05, 0.01];
-
+  //push the amount of cash in drawer into it's own array to simplify
   for (var i = 0; i < cid.length; i++){
     drawerCash.push(cid[i][1]);
   }
   
+  //if the amount of total cash is equal to the amount of change, return Closed
   var totalCash = drawerCash.reduce(add, 0);
     function add(a, b) {
     return a + b;
@@ -21,28 +24,19 @@ function checkCashRegister(price, cash, cid) {
     return 'Closed';
   }
   
+  //starting from largest denomination, check if change is divisible
   for (var j = 0; j < drawerCash.length; j++){
-    if (change%denom[j] === 0){
-      if (change <= drawerCash[j]){
-        cid[j][1] = change;
-        ans.push(cid[j]);
-        return ans;
-      }
-      else{
-        return 'Insufficient Funds';
-      }
-    }
-    else if (change/denom[j] >= 1){
+    if (change/denom[j] >= 1){
       var bill;
+      // if the change is divisible by a denomination keep subtracting the change from cash in drawer until it is no longer divisible or there are no more cash in the drawer.
       for (var k = 1; change/denom[j] >= 1 && drawerCash[j] > 0; k++){
-        drawerCash[j] = drawerCash[j]- denom[j];
-        change = change - denom[j];
-        change = change.toFixed(2);
+        drawerCash[j] = drawerCash[j] - denom[j];
+        change = (change- denom[j]).toFixed(2);
         bill = k;
       }
+      //
       if (cid[j][1] > 0){
           cid[j][1] = denom[j]*bill;
-
           ans.push(cid[j]);
       }
       else {
@@ -52,7 +46,6 @@ function checkCashRegister(price, cash, cid) {
   }
   return ans;
 }
+
 //change = 96.74 drawerCash [100, 60, 20, 55, 90, 4.25, 3.10, 2.05, 1.01] 
 checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
-@Andifer702
-Commit changes
