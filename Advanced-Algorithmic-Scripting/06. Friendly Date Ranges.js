ec6a875967a79ae73e712932e37eadbefe785dd5
Joon Park <apark702@gmail.com>
2016-04-09 13:38:34 -0400

    Update 06. Friendly Date Ranges.js

--- Advanced-Algorithmic-Scripting/06. Friendly Date Ranges.js
+++ Advanced-Algorithmic-Scripting/06. Friendly Date Ranges.js
@@ -1,15 +1,34 @@
 function makeFriendlyDates(arr) {
   var begDate = new Date(arr[0].split('-')),
-      endDate = new Date(arr[1].split('-'));
+      endDate = new Date(arr[1].split('-')),
+      begYear = begDate.getFullYear(),
+      endYear = endDate.getFullYear(),
+      begMonth = begDate.getMonth(),
+      endMonth = endDate.getMonth(),
+      begDays = begDate.getUTCDate(),
+      endDays = endDate.getUTCDate(),
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   
-  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  
+  function corDate(n){
+    if (n === 1 || n === 21 || n === 31){
+      return n + 'st';
+    }
+    else if (n === 2 || n === 22){
+        return  n + 'nd';
+        }
+    else if (n === 3 || n === 23){
+      return  n + 'rd';
+    }
+    else {
+      return n + 'th'
+    }
+  }
   if (begDate.getTime() === endDate.getTime()){
-    return [months[begDate.getMonth()] + ' ' + begDate.getDate() + 'th' + "," + ' ' + begDate.getUTCFullYear()];
+    return [months[begMonth] + ' ' + corDate(begDays) + "," + ' ' + begYear];
   }
   
   else if (begDate.getMonth() === endDate.getMonth() && begDate.getFullYear() === endDate.getFullYear()){
-    return [months[begDate.getUTCMonth()] + ' ' + begDate.getUTCDate() + 'st' ,  endDate.getUTCDate() + 'th'];
+    return [months[begDate.getUTCMonth()] + ' ' + corDate(begDays) ,  endDate.getUTCDate() + 'th'];
   }
 }
 
