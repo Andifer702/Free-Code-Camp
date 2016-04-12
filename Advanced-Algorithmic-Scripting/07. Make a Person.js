b6b74fd9a2282298bd46a9ad8722e64608b0bd7f
Joon Park <apark702@gmail.com>
2016-04-12 16:17:17 -0400

    Update 07. Make a Person.js

--- Advanced-Algorithmic-Scripting/07. Make a Person.js
+++ Advanced-Algorithmic-Scripting/07. Make a Person.js
@@ -1,16 +1,34 @@
 var Person = function(firstAndLast) {
-    firstAndLast.split(' ');
-    var firstName = firstAndLast[0],
-        lastName = firstAndLast[1];
-  getFirstName = function() {
+    firstAndLast = firstAndLast.split(' ');
+    var firstName = firstAndLast[0]; //assign first and last names separately
+    var lastName = firstAndLast[1];
+    var fullName = firstAndLast[0] + ' ' + firstAndLast[1];
+  
+  //method for getting the first, last, or full name of the person
+  this.getFirstName = function() {
     return firstName;
     };
-  getLastName = function() {
+  this.getLastName = function() {
     return lastName;
   };
-  getFullName = function() {
+  this.getFullName = function() {
     return firstName + ' ' + lastName;
   };
+  
+  //Setting new methods for changing name
+  this.setFirstName = function(name) {
+    firstName = name;
+  };
+  this.setLastName = function(name) {
+    lastName = name;
+  };
+  this.setFullName = function(name){
+    name = name.split(' ');
+    firstName = name[0];
+    lastName = name[1];
+  };
+  
 };
 
 var bob = new Person('Bob Ross');
+bob.getFirstName();
