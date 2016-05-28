674e67d9f613bb33b0ebdb1061122f2827920e0e
Joon Park <apark702@gmail.com>
2016-05-28 11:44:01 -0400

    Update index.js

--- Advanced-Front-End-Development-Project/JavaScript-Calculator/js/index.js
+++ Advanced-Front-End-Development-Project/JavaScript-Calculator/js/index.js
@@ -1,85 +1,115 @@
-var eq = '';
-var operand = ['*', "/", '+','-', '%'];
-function adding(button){
+$(document).ready(function() {
+  var eq = 'x';
+  var operand = ['*', "/", '+', '-', '%'];
   
-}
+  //check if the input has been already evaluated. If so, number entries will start a new entry, others will add on to exsisting.
+  function newEntry(){
+    if (eq === 'o'){
+      $('#box').val('');
+      eq = 'x'
+    }
+  };
+  
+  //button inputs
+  $('#7').click(function() {
+    newEntry();
+    $('#box').val($('#box').val() + '7')
+  });
 
-$('#7').click(function(){
-  $('#box').val($('#box').val() + '7')
-});
+  $('#8').click(function() {
+    newEntry();
+    $('#box').val($('#box').val() + '8')
+  });
 
-$('#8').click(function(){
-  $('#box').val($('#box').val() + '8')
-});
+  $('#9').click(function() {
+    newEntry();
+    $('#box').val($('#box').val() + '9')
+  });
 
-$('#9').click(function(){
-  $('#box').val($('#box').val() + '9')
-});
+  $('#mult').click(function() {
+    $('#box').val($('#box').val() + '*')
+    eq = 'x'
+  });
 
-$('#mult').click(function(){
-  $('#box').val($('#box').val() + '*')
-});
+  $('#divide').click(function() {
+    $('#box').val($('#box').val() + '/')
+    eq = 'x'
+  });
 
-$('#divide').click(function(){
-  $('#box').val($('#box').val() + '/')
-});
+  $('#4').click(function() {
+    newEntry();
+    $('#box').val($('#box').val() + '4')
+  });
 
-$('#4').click(function(){
-  $('#box').val($('#box').val() + '4')
-});
+  $('#5').click(function() {
+    newEntry();
+    $('#box').val($('#box').val() + '5')
+  });
 
-$('#5').click(function(){
-  $('#box').val($('#box').val() + '5')
-});
+  $('#6').click(function() {
+    newEntry();
+    $('#box').val($('#box').val() + '6')
+  });
 
-$('#6').click(function(){
-  $('#box').val($('#box').val() + '6')
-});
+  $('#add').click(function() {
+    $('#box').val($('#box').val() + '+')
+    eq = 'x'
+  });
 
-$('#add').click(function(){
-  $('#box').val($('#box').val() + '+')
-});
+  $('#minus').click(function() {
+    $('#box').val($('#box').val() + '-')
+    eq = 'x'
+  });
 
-$('#minus').click(function(){
-  $('#box').val($('#box').val() + '-')
-});
+  $('#1').click(function() {
+    newEntry();
+    $('#box').val($('#box').val() + '1')
+  });
 
-$('#1').click(function(){
-  $('#box').val($('#box').val() + '1')
-});
+  $('#2').click(function() {
+    newEntry();
+    $('#box').val($('#box').val() + '2')
+  });
 
-$('#2').click(function(){
-  $('#box').val($('#box').val() + '2')
-});
+  $('#3').click(function() {
+    newEntry();
+    $('#box').val($('#box').val() + '3')
+  });
 
-$('#3').click(function(){
-  $('#box').val($('#box').val() + '3')
-});
+  $('#opP').click(function() {
+    newEntry();
+    $('#box').val($('#box').val() + '(')
+  });
 
-$('#opP').click(function(){
-  $('#box').val($('#box').val() + '(')
-});
+  $('#clP').click(function() {
+    newEntry();
+    $('#box').val($('#box').val() + ')')
+  });
 
-$('#clP').click(function(){
-  $('#box').val($('#box').val() + ')')
-});
+  $('#0').click(function() {
+    newEntry();
+    $('#box').val($('#box').val() + '0')
+  });
 
-$('#0').click(function(){
-  $('#box').val($('#box').val() + '0')
-});
+  $('#dot').click(function() {
+    newEntry();
+    $('#box').val($('#box').val() + '.')
+  });
+  
+  //percentage calculation. evaluates the entry first before accepting input.
+  $('#perc').click(function() {
+    $('#box').val(eval($('#box').val()));
+    $('#box').val(eval($('#box').val()/100));
+    eq = 'o'
+  });
 
-$('#dot').click(function(){
-  $('#box').val($('#box').val() + '.')
-});
+  //evaluates the entry then set the eq variable for the new entries.
+  $('#eq').click(function(){
+    $('#box').val(eval($('#box').val()));
+    eq = 'o';
+  });
 
-$('#perc').click(function(){
-  $('#perc').val(eval($('#box').val())/100)
-});
-
-$('#eq').click(function(){
-  $('#box').val(eval($('#box').val())
-)});
-
-$('#AC').click(function(){
-  $('#box').val(' ')
+  $('#AC').click(function() {
+    $('#box').val('')
+  });
 });
