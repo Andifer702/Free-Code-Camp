5a00364ee804255c577ef72438b0a8def8de0352
Joon Park <apark702@gmail.com>
2016-08-19 17:17:18 -0400

    Update 03. Check for Palindromes.js

--- Basic-Algorithmic-Scripting/03. Check for Palindromes.js
+++ Basic-Algorithmic-Scripting/03. Check for Palindromes.js
@@ -1,3 +1,5 @@
+//Return true if the given string is a palindrome. Otherwise, return false.
+
 function palindrome(str) {
   // simplify the str to all lower case and special characters removed
   var strippedStr = str.toLowerCase().replace(/\W|_/g, ''); 
