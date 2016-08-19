4e2f05d1fa85799c0afc4877a1d1c33be1c69425
Joon Park <apark702@gmail.com>
2016-08-19 17:21:20 -0400

    Update 03. Exact Change.js

--- Advanced-Algorithmic-Scripting/03. Exact Change.js
+++ Advanced-Algorithmic-Scripting/03. Exact Change.js
@@ -1,3 +1,8 @@
+//Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+//cid is a 2D array listing available currency.
+//Return the string "Insufficient Funds" if cash-in-drawer is less than the change due. 
+//Return the string "Closed" if cash-in-drawer is equal to the change due.
+
 function checkCashRegister(price, cash, cid) {
   var change;
   var drawerCash = [];
