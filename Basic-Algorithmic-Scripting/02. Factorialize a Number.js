19a98ad899cf80e39d5a7c37d29a30d4ec73faca
Joon Park <apark702@gmail.com>
2016-08-19 17:16:17 -0400

    Update 02. Factorialize a Number.js

--- Basic-Algorithmic-Scripting/02. Factorialize a Number.js
+++ Basic-Algorithmic-Scripting/02. Factorialize a Number.js
@@ -1,3 +1,7 @@
+//Return the factorial of the provided integer.
+//If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+//Factorials are often represented with the shorthand notation n!
+
 function factorialize(num) {
   var factorial = 1;
   //multiply the number by the factorial then reduce the number by 1 and repeat until number is reduced to zero
