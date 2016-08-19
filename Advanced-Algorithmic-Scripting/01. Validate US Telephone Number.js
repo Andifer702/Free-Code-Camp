123123f35eb4fe941b19e0dad79a746ab98b30bc
Joon Park <apark702@gmail.com>
2016-08-19 17:19:02 -0400

    Update 01. Validate US Telephone Number.js

--- Advanced-Algorithmic-Scripting/01. Validate US Telephone Number.js
+++ Advanced-Algorithmic-Scripting/01. Validate US Telephone Number.js
@@ -1,3 +1,9 @@
+//Return true if the passed string is a valid US phone number.
+//The user may fill out the form field any way they choose as long as it is a valid US number. 
+//The following are examples of valid formats for US numbers (refer to the tests below for other variants)
+//555-555-5555 (555)555-5555 (555) 555-5555 555 555 5555 5555555555 1 555 555 5555
+
+
 function telephoneCheck(str) {
   // Good luck!
   var regexp = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?(\d{3})?[\s\-]?(\d{4})$/g;
